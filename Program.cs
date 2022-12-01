0992e8921c02f05a777276d89dc078335e4c8579
rajpal229 <rajpalrawat229@gmail.com>
2022-12-01 19:59:18 +0530

    Added: Calculating Length of Line

--- Program.cs
+++ Program.cs
@@ -1,10 +1,25 @@
-﻿namespace LineComparisonProblem
+﻿using System.Data.SqlTypes;
+using System.Transactions;
+
+namespace LineComparisonProblem
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Line Comparison Problem");
+            Console.WriteLine("\n");
+            Console.WriteLine("Enter End points of Line");
+            Console.WriteLine("Enter x1: ");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y1: ");
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter x2: ");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter y2: ");
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            double length = Math.Sqrt(Math.Pow((x2 - x1), 2) + Math.Pow((y2 - y1), 2));
+            Console.WriteLine("Length of Line is: " + length);
         }
     }
 }
